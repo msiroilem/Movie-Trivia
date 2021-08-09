1ed72040292bedf06969a6e705b00d7a03da0a0e
msiroilem <twjones86@gmail.com>
2021-08-09 15:08:46 -0400

    elements on HTML pages

---
+++ script.js
@@ -0,0 +1,4 @@
+const playGameButton = document.querySelector('.playGameBtn')
+const answerFields = document.querySelectorAll('.answer-fields')
+const questionField = document.querySelectorAll('#question-field')
+const highScore = document.querySelector('highScore')
