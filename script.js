fab6328cb6a1aae6ef81b5286a54bc451781dfba
msiroilem <twjones86@gmail.com>
2021-08-09 17:11:05 -0400

    started questions/answers arrays

--- script.js
+++ script.js
@@ -5,11 +5,37 @@ const answerFields = document.querySelectorAll('.answer-fields')
 const questionField = document.querySelectorAll('#question-field')
 const highScore = document.querySelector('.high-score')
 const restartButton = document.querySelectorAll('#restart')
+const questions = [
+  'What was the release year for Alien?',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' '
+]
+const answers = [
+  ['1978', '1979', '1980', '1981'],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ']
+]
+
+const correctAnswers = []
 
 // Event Listeners
 
 for (let i = 0; i < answerFields.length; i++) {
-  answerFields.addEventListener
+  answerFields.addEventListener('click')
 }
 
 function restart() {
