2f1b9c5c9ade6224b1f95f1a31c7a6b2538ad3c8
msiroilem <twjones86@gmail.com>
2021-08-09 16:05:29 -0400

    DOM and event listeners partially added

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// Global Variables
+
 const playGameButton = document.querySelector('.playGameBtn')
 const answerFields = document.querySelectorAll('.answer-fields')
 const questionField = document.querySelectorAll('#question-field')
-const highScore = document.querySelector('highScore')
+const highScore = document.querySelector('.high-score')
+const restartButton = document.querySelectorAll('#restart')
+
+// Event Listeners
+
+for (let i = 0; i < answerFields.length; i++) {
+  answerFields.addEventListener
+}
+
+function restart() {
+  location: reload()
+  restartButton.addEventListener('click', restart)
+}
