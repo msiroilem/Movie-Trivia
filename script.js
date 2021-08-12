ddab9313ac591c696060d2b4a1348e32af193885
msiroilem <twjones86@gmail.com>
2021-08-12 17:28:34 -0400

    cleaned up js/edited css a smidge

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let currentQuestion = 0
 const playGameButton = document.querySelector('.play-game-button')
 const answerFields = document.querySelector('.answer-fields')
 const questionField = document.querySelector('#question-field')
-
 const playAgainBtn = document.querySelector('#restart')
 const paragraph = document.querySelector('p')
+const h3 = document.querySelector('h3')
 
 const questions = [
   'What was the release year for the Ridley Scott directed movie, Alien?',
@@ -50,19 +50,19 @@ const checkAnswers = (btn, answer) => {
     answers[currentQuestion].indexOf(answer) === correctAnswers[currentQuestion]
   ) {
     score++
-    paragraph.innerText = `Correct!`
     btn.style.backgroundColor = 'green'
+    btn.innerText = `Correct!`
     disableButtons()
     setTimeout(() => {
       nextPage()
-    }, 500)
+    }, 1000)
   } else {
-    paragraph.innerText = `Oh no! You're incorrect!`
     btn.style.backgroundColor = 'red'
+    btn.innerText = `Oh no! You're incorrect!`
     disableButtons()
     setTimeout(() => {
       nextPage()
-    }, 500)
+    }, 1000)
   }
   updateScore()
 }
@@ -94,6 +94,7 @@ function nextPage() {
     currentQuestion += 1
   } else {
     document.getElementById('restart').classList.remove('hidden')
+    let userResponse
     if (userResponse === true) {
       restart()
     }
